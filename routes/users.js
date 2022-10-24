be374725f55dbbf879dc3bbb7c994a499e5de01d
neerazveeranki <44938842+neerazveeranki@users.noreply.github.com>
2022-10-24 11:40:19 -0500

    modified users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var times = 0
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(`User accesses are: ${times++}`);
 });
 
 module.exports = router;
